40946df9453fde74420b5f1879914409d8c345c4
Nash <nash.skinner@yahoo.com>
2022-02-17 14:24:12 -0600

    Addition of oprimalMaxWeight, but RecursiveMaxWeight still goes outside of memory.

--- HW2/HW2_1.cpp
+++ HW2/HW2_1.cpp
@@ -3,72 +3,123 @@
 #include <stdlib.h> 
 #include <time.h>
 #include <array>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
-const int n = 500;
+void fillArray(int* a, int l) {
+    for (int i = 0; i < l; ++i) {
+        a[i] = (rand() % 1998 + (-999));
+    }
+}
 
-class ArrayFuctions {
-    public:
-        array<int, n> myArray;
+// Print out an Array.
+void readArray(int A[], int l) {
+    cout << "Reading elements.\n";
+    for (int i = 0; i < l; ++i) {
+        cout << A[i] << " , ";
+        if(i != 0 && i % 9 == 0) 
+            cout << endl;
+    }
+    cout << endl;
+}
 
-    // Fill in an Array of length n with values -999 to 999.
-    void fillArray() {
-            for (int i = 0; i <= n; ++i) {
-                myArray[i] = (rand() % 1998 + (-999));
-                //myArray[i] = (rand() % 10 + (-5));
-            }
+// Linear Max Weight anaylysis of subsets of an Array.
+int iterativeMaxWeight(int A[], int l) {
+    // If the array is empty there is no weight
+    if (l == 0) {
+        return 0;
     }
 
-    // Print out an Array.
-    void readArray() {
-        cout << "Reading elements.\n";
-        for (int i = 0; i <= n; ++i) {
-            cout << myArray[i] << " , ";
-            if(i != 0 && i % 10 == 0) {
-                cout << endl;
-            }
+    int maxSum = 0;
+    for(int i = 0; i < l; ++i) {
+        for(int j = i; j <= l; ++j) {  
+            int sum = 0;
+            for(int k = i; k <= j; ++k)
+                sum = sum + A[k];
+            if(sum > maxSum)
+                maxSum = sum;
         }
+    } 
+    return(maxSum);
+}
+
+// Compute the Crossing Sum for the function recursive Max Weight
+int maxCrossingSum(int currentArray[], int low, int mid, int r) {
+    int sum = 0;
+    int lsum = INT_MIN;
+    for(int i = mid; i > low; --i) {
+        sum = sum + currentArray[i];
+        if (sum > lsum)
+            lsum = sum;
     }
+    sum = 0;
+    int rsum = INT_MIN;
+    for(int i = mid+1; i < r; ++i) {
+        sum = sum + currentArray[i];
+        if (sum > rsum)
+            rsum = sum;
+    }
+    return (lsum + rsum);
+}
 
-    // Linear Max Weight anaylysis of subsets of an Array.
-    int iterativeMaxWeight() {
-        // If the array is empty there is no weight
-        if (myArray.empty()) {
-            return 0;
-        }
+// Recursive Max Weight anaylysis of subsets of an Array.
+int recursiveMaxWeight(int A[], int low, int high) {
+    if (low == high)
+        return A[low];
+    else {
+        int mid = low + floor ((high - low)/2);
+        int left_sum = recursiveMaxWeight (A, low, mid);
+        int right_sum = recursiveMaxWeight (A, mid+1, high);
+        int crossing_Sum = maxCrossingSum(A, low, mid, high);
 
-        int maxSum = 0;
-        for(int i = 0; i <= n; ++i) {
-            for(int j = i; j <= n; ++j) {  
-                int sum = 0;
-                for(int k = i; k <= j; ++k)
-                    sum = sum + myArray[k];
-                if(sum > maxSum)
-                    maxSum = sum;
-            }
+        cout << left_sum << " , " << crossing_Sum << " , " << right_sum << endl;
+
+        if(left_sum > right_sum && left_sum > crossing_Sum) {
+            return left_sum;
         } 
-
-        return(maxSum);
+        if (right_sum > crossing_Sum){
+            return right_sum;
+        }
+        else{
+            return(crossing_Sum);   
+        }
     }
+}
 
-    int recursiveMaxWeight() {
-        return 0;
+// Optimal Max Weight anaylysis of subsets of an Array.
+int optimalMaxWeight(int A[], int n) {
+    int max_so_far = 0;
+    int max_ending_here = 0;
+    for (int i = 0; i < n-1; ++i) {
+        max_ending_here += A[i];
+        
+        if( max_ending_here < 0 )
+            max_ending_here = 0;
+        
+        max_so_far = max(max_so_far, max_ending_here);
     }
-
-    int optimalMaxWeight() {
-        return 0;
-    }
-};
+   return max_so_far;
+}
 
 
 int main() {
     srand (time(NULL));
 
-    ArrayFuctions startArray;
+    int length = 0;
+    cout << "Give the max length (> 500): ";
+    cin >> length;
 
-    startArray.fillArray();
-    startArray.readArray();
-    cout << startArray.iterativeMaxWeight() << endl;
+    if(length <= 0 or length > 500) {
+        cout << "Rerun program with value less than or equal to 500."  << endl;
+    } else {
+        int currentArray[length];
+        fillArray(currentArray, length);
 
+        readArray(currentArray, length);
+        cout << iterativeMaxWeight(currentArray, length) << endl;
+        cout << recursiveMaxWeight(currentArray, 0, length - 1) << endl;
+        cout << optimalMaxWeight(currentArray, length) << endl;
+    }
     return 0;
 }
