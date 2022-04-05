45b58da1197cb70e3ddcff981128fe87a7390ede
Nash-Skinner <nash.skinner@yahoo.com>
2022-04-04 20:38:18 -0500

    Beautify + Pre and Post

--- HW3/HeapSort.cpp
+++ HW3/HeapSort.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-using namespace std;
-  
-// function to heapify the tree
-void heapify(int arr[], int n, int root) {
+#include <time.h>
+
+ /*
+    Utility Function - to fill an array with a spread of values
+    Pre: An arbitrary array length n 
+    Post: Returns an array with arbirary length carrying numbers 0 to 999
+ */ 
+void fillArray(int* arr, int n) {
+    for (int i = 0; i < n; ++i) {
+        arr[i] = (rand() % 999);
+    }
+}
+
+/* 
+    Utility Function - to print array by powers of 2
+    Pre: An arbitrary array length n 
+    Post: A heap display printed to console
+*/
+void displayArray(int* arr, int n) {
+    int currPow2 = 0;
+
+    for (int i=0; i<n; ++i){
+        if(i >= currPow2) {
+            std::cout << std::endl;
+            currPow2 = currPow2 * 2 + 1;
+        }
+        std::cout << arr[i] << " ";
+    }
+
+   std::cout << "\n\n";
+}
+
+/*
+    Function to heapify the tree
+    Pre: An arbitrary array length n with known root
+    Post: Returns heap_array length n
+*/
+void heapify(int* arr, int n, int root) {
     int largest = root; // root is the largest element
     int l = 2*root + 1; // left = 2*root + 1
     int r = 2*root + 2; // right = 2*root + 2
@@ -18,15 +52,19 @@ void heapify(int arr[], int n, int root) {
     // If largest is not root
     if (largest != root) {
         //swap root and largest
-        swap(arr[root], arr[largest]);
+        std::swap(arr[root], arr[largest]);
   
         // Recursively heapify the sub-tree
         heapify(arr, n, largest);
     }
 }
   
-// implementing heap sort
-void heapSort(int arr[], int n) {
+/*
+    Implementing heap sort
+    Pre: An arbitrary array length n 
+    Post: Returns a built: numberically sorted heap_heap based on the lowest value
+*/
+void heapSort(int* arr, int n) {
     // build heap
     for (int i = n / 2 - 1; i >= 0; i--)
     heapify(arr, n, i);
@@ -34,29 +72,30 @@ void heapSort(int arr[], int n) {
     // extracting elements from heap one by one
     for (int i=n-1; i>=0; i--) {
         // Move current root to end
-        swap(arr[0], arr[i]);
-  
+        std::swap(arr[0], arr[i]);
+
         // again call max heapify on the reduced heap
         heapify(arr, i, 0);
     }
 }
   
-/* print contents of array - utility function */
-void displayArray(int arr[], int n) {
-    for (int i=0; i<n; ++i)
-        cout << arr[i] << " ";
-   cout << "\n";
-}
-  
-// main program
+/* main program */
 int main() {
-    int heap_arr[] = {4,17,3,12,9,6};
-    int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
-    cout<<"Input array"<<endl;
-    displayArray(heap_arr,n);
+    // Initalized random valueation 
+    srand(time(NULL));
+
+    int length = 0;
+    std::cout << "Give the max length: ";
+    std::cin >> length;
+
+    int heap_arr[length];
+    fillArray(heap_arr, length);
+
+    std::cout<<"Input array:"<<std::endl;
+    displayArray(heap_arr,length);
   
-    heapSort(heap_arr, n);
+    heapSort(heap_arr, length);
   
-    cout << "Sorted array"<<endl;
-    displayArray(heap_arr, n);
+    std::cout << "Sorted array:"<<std::endl;
+    displayArray(heap_arr, length);
 }
